cbd25e851bb48082947cf5640af23550e9d12d9c
Otto <ottogutierrez@me.com>
2024-01-27 00:22:50 -0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <header>
+        <h1>TaskTango</h1>
+        <p className="tagline">
+          Where your tasks dance to completion
+        </p>
+        
+      </header>
+    </>
+  );
+}
+
+export default App;
